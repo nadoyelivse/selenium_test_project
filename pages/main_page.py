a7df728cdfbdcd970306f9d7e805120568a6699e
”nadoyelivse” <sidorovaksusa95@gmail.com>
2025-10-02 15:55:54 +0300

    Реализация перехода страницы в теле текста

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 from .base_page import BasePage
 from .locators import MainPageLocators
+from .login_page import LoginPage
 
 class MainPage(BasePage): 
     def go_to_login_page(self):
@@ -9,3 +10,7 @@ class MainPage(BasePage):
 
     def should_be_login_link(self):
         assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
+
+    def go_to_login_page(self):
+    link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+    link.click()
--- test_main_page.py
+++ test_main_page.py
@@ -4,19 +4,23 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
-from pages.locators import MainPageLocators, LoginPageLocators  # ДОБАВЬ LoginPageLocators!
+from pages.locators import MainPageLocators, LoginPageLocators  
 from selenium.webdriver.common.by import By
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
     page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
     
     login_link = browser.find_element(*MainPageLocators.LOGIN_LINK)
     login_link.click()
     
     login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()  # Этот метод использует LoginPageLocators
+    login_page.should_be_login_page()  
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
